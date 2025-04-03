c1eeec6e809a71e5b8472277a33193b0ed16d44d
EstherNgeno <estherngeno8@gmail.com>
2025-04-03 15:44:10 +0300

    first project section

--- src/components/sections/Projects.js
+++ src/components/sections/Projects.js
@@ -1,4 +1,8 @@
 import React from "react";
+import Lateshow from "../../assets/Lateshow.png";
+import { AiFillGithub } from "react-icons/ai";
+import { FaArrowUpRightFromSquare } from "react-icons/fa6";
+import { AiFillYoutube } from "react-icons/ai";
 
 
 const Projects = () => {
@@ -8,14 +12,39 @@ const Projects = () => {
             <div className="flex flex-col justify-start items-start lg:items-start lg:text-left pl-40 lg:pl-40 text-black dark:text-white max-w-lg">
             <h1 className="text-4xl sm:text-5xl font-semibold ml-20 mb-4 leading-tight">{`<Projects/>`}</h1>
             </div>
-            <div className="flex flex-col justify-start items-start lg:items-start lg:text-left pl-40 lg:pl-40 text-black dark:text-white max-w-lg">
-                <ul className="animate-in flex flex-col animated-list">
-                    <li className="py-6 flex flex-col md:flex-row gap-4 md:gap-6 transition-opacity first:pt-0 last:pb-0">
-                        <a className="w-full md:w-2/5 aspect-video bg-tertiary rounded-lg border border-secondary overflow-clip select-none" href="https://lateshow-esther-ngeno.onrender.com/"></a>
-                    </li>
-
-                </ul>
+            <div className="grid grid-cols-1 gap-3 sm:grid-cols-2 max-w-[1100px] justify-start items-start lg:items-start pl-40 lg:pl-40 ml-20 pt-10 text-black dark:text-white">
+            <div className="rounded-lg bg-card text-card-foreground flex flex-col overflow-hidden border hover:shadow-lg transition-all duration-300 ease-out h-full">
+                <a className="block cursor-pointer" href="https://lateshow-esther-ngeno.onrender.com/">
+                <img alt="Lateshow" className="h-50 w-full overflow-hidden object-cover object-top" src={Lateshow}/>
+                </a>
+                <div className="flex flex-col px-2">
+                    <div className="prose max-w-full text-pretty text-base text-muted-foreground">
+                        <p className="prose max-w-full text-pretty text-base text-muted-foreground">This is a code challenge where we are building an API to track shows and their ratings.</p>
+                    </div>
+                </div>
+                <div className="text-base text-muted-foreground mt-auto flex flex-col px-2">
+                    <div className="mt-2 flex flex-wrap gap-1">
+                        <div className="inline-flex items-center rounded-md border font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 border-transparent bg-gray-300 dark:text-black px-1 py-0 text-[10px]">Flask</div>
+                        <div className="inline-flex items-center rounded-md border font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 border-transparent bg-gray-300 dark:text-black px-1 py-0 text-[10px]">SQLAlchemy</div>
+                        <div className="inline-flex items-center rounded-md border font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 border-transparent bg-gray-300 dark:text-black px-1 py-0 text-[10px]">Python</div>
+                    </div>
+                </div>
+                <div className="flex items-center pt-2 px-2 pb-2">
+                    <div className="flex flex-row flex-wrap items-start gap-1">
+                        <a href="https://github.com/Moringa-SDF-PTO7/lateshow-esther-ngeno">
+                        <div className="items-center rounded-md border font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 bg-fuchsia-100 text-black flex gap-2 px-2 py-1 text-[12px]"> <AiFillGithub className="w-7 h-7" /> GitHub</div>
+                        </a>
+                        <a href="https://lateshow-esther-ngeno.onrender.com/">
+                        <div className="items-center rounded-md border font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 bg-fuchsia-100 text-black flex gap-2 px-2 py-1 text-[12px]"> <FaArrowUpRightFromSquare className="w-7 h-7" /> Live Link</div>
+                        </a>
+                        <a href="https://lateshow-esther-ngeno.onrender.com/">
+                        <div className="items-center rounded-md border font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 bg-fuchsia-100 text-black flex gap-2 px-2 py-1 text-[12px]"> <AiFillYoutube className="w-7 h-7" /> YouTube</div>
+                        </a>
+                    </div>
+                </div>
             </div>
+            </div>
+
 
             </section>
     
